c8b76bb4b12cb60fd396d7b4b4aa37492da655d4
crpboy <crpboy2004@gmail.com>
2025-05-14 21:42:08 +0800

    fix(la64): support strlen for la64 linker

--- libd/src/arch/la64/la_libc_import.rs
+++ libd/src/arch/la64/la_libc_import.rs
@@ -9,3 +9,17 @@ pub extern "C" fn _Unwind_Resume() {}
 
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
+
+#[inline]
+unsafe fn my_strlen(p: *const u8) -> usize {
+    let mut n = 0;
+    while *p.offset(n as isize) != 0 {
+        n += 1;
+    }
+    n
+}
+
+#[no_mangle]
+pub extern "C" fn strlen(p: *const u8) -> usize {
+    unsafe { my_strlen(p) }
+}
